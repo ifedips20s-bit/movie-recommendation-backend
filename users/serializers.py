7d8383a3788d0aa379925ee405306f6beb69ff96
ifedips20s <ifedips20s@gmail.com>
2026-01-22 11:38:24 +0100

    feat: implement custom user model and JWT authentication

---
+++ users/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import CustomUser
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email']
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        return token
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import CustomUser
+from .serializers import UserSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+class UserCreateView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
